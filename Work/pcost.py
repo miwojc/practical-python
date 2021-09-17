5b4661ad7fcfc4e2e074a61a37ec2fa4825c444d
mwojc <miwojc@pm.me>
2021-09-17 20:02:54 +0200

    1.19-1.28

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,10 @@
 # pcost.py
-#
-# Exercise 1.27
+
+with open('Data/portfolio.csv', 'rt') as f:
+    headers = next(f).split(',')
+    total = 0
+    for line in f:
+        _, shares, price = line.split(',')
+        total += int(shares) * float(price)
+
+print(f'Total cost {total:.2f}')
