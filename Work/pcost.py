95801c247281671173644dce52f6d8a7b1681e6e
mwojc <miwojc@pm.me>
2021-11-03 08:13:39 +0100

    3.11-3.14

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,21 +1,14 @@
 # pcost.py
-import csv
 import sys
 
+import report
+
 def portfolio_cost(filename):
-    with open(filename, 'rt') as f:
-        rows = csv.reader(f)
-        headers = next(rows)
-        total = 0
-        for rowno, row in enumerate(rows, start=1):
-            record = dict(zip(headers, row))
-            try:
-                nshares = int(record['shares'])
-                price = float(record['price'])
-                total += nshares * price
-            except ValueError:
-                print(f"Row {rowno}: Bad row: {row}")
-        return total
+    ''' 
+    Computes the total cost (shares*price) of a portfolio file
+    ''' 
+    portfolio = report.read_portfolio(filename)
+    return sum([s['shares']*s['price'] for s in portfolio])
 
 if len(sys.argv) == 2:
     filename = sys.argv[1]
