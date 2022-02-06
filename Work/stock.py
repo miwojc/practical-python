79fd5fc59a2f74044d3e6747f5e470b2997dcab3
miwojc <miwojc@protonmail.com>
2022-02-06 08:36:40 +0100

    5.1-5.8

--- Work/stock.py
+++ Work/stock.py
@@ -1,11 +1,21 @@
+# stock.py
+
 class Stock:
     '''
     An instance of stock holding consisting of name, shares, and price
     '''
+    __slots__ = ('name', '_shares', 'price')
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
+        
+    @property
+    def cost(self):
+        '''
+        Return the cost as shares*price
+        '''
+        return self.shares * self.price
 
     def sell(self, amount):
         '''
@@ -13,11 +23,15 @@ class Stock:
         '''
         self.shares -= amount
 
-    def cost(self):
-        '''
-        Return the cost as shares*price
-        '''
-        return self.shares * self.price
+    @property
+    def shares(self):
+        return self._shares
+        
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, int):
+            raise TypeError('Expected int')
+        self._shares = value
 
     def __repr__(self):
         return f"Stock('{self.name}', {self.shares}, {self.price})"
