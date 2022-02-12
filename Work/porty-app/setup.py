e28e8bbbbcfe4bf4c4daa47ef52aab8622e43cad
miwojc <miwojc@protonmail.com>
2022-02-12 09:03:31 +0100

    9.4-9.5

---
+++ Work/porty-app/setup.py
@@ -0,0 +1,12 @@
+# setup.py
+
+import setuptools
+
+setuptools.setup(
+	name="porty",
+	version="0.0.1",
+	author="miwojc",
+	author_email="miwojc@example.com",
+	description="Practical Python Code",
+	packages=setuptools.find_packages(),
+)
